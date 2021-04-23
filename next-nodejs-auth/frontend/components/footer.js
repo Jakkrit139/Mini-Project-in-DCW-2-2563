8dde13db4036aeec88e04836130e7b9a472b2ce1
Jakkrit139 <Jakkrit36935@gmail.com>
2021-04-23 20:28:32 +0700

    one two commit

--- next-nodejs-auth/frontend/components/footer.js
+++ next-nodejs-auth/frontend/components/footer.js
@@ -5,7 +5,24 @@ import styles from  "../styles/Home.module.css";
     const Footer = () => (
 
     <div className={styles.footer} >
-       
+         
+         <div class="center box">
+          <h2>Address</h2>
+          <div class="content">
+            <div class="place">
+              <span class="fas fa-map-marker-alt"></span>
+              <span class="text">Birendranagar, Surkhet</span>
+            </div>
+            <div class="phone">
+              <span class="fas fa-phone-alt"></span>
+              <span class="text">+089-765432100</span>
+            </div>
+            <div class="email">
+              <span class="fas fa-envelope"></span>
+              <span class="text">abc@example.com</span>
+            </div>
+          </div>
+        </div>
     </div>
 )
 
